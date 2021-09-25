c731ea0426e0df42dfe3bc67174d2463415401d1
abuAbdirRahman <abuAbdirRahman@edd31041-f016-4b03-a3df-3ae45175ec67>
2021-09-25 04:49:42 +0000

    AssemblyCode.java has been updated with Streams And Collections alhamdulillah

--- com/salikoon/emulator8086/user_code/AssemblyCode.java
+++ com/salikoon/emulator8086/user_code/AssemblyCode.java
@@ -1,33 +1,29 @@
 // بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيْمِ
 
 package com.salikoon.emulator8086.user_code;
+import java.util.ArrayList;
+
 public class AssemblyCode 
 {
-    private static String[] userCode;
+    private static ArrayList<String> userCode;
     
     public static void setCode(String[] input)
     {
-          userCode=input;
-          trimAllLinesOfUserCode();
-         convertToUpperCaseAllLinesOfUserCode();
-       
+         userCode=
+         java.util.Arrays.
+          stream(input).parallel()
+          .map(String::strip)
+          .map(String::toUpperCase)
+          .collect
+          (java.util.stream.Collectors
+          .toCollection(ArrayList::new)
+          );
     }
     public static String getCode(int lineNumber)
     {
-        return userCode[lineNumber];
+        return userCode.get(lineNumber);
     }
-    private static void trimAllLinesOfUserCode()
-    {
-    	for(int index=0; index<userCode.length; index++)
-    	userCode[index]=userCode[index].trim();
-    	
-    }
-    private static void convertToUpperCaseAllLinesOfUserCode()
-    {
-    		for(int index=0; index<userCode.length; index++)
-    	userCode[index]=userCode[index].toUpperCase();
- 
-    	
-    }
- } //end of class
+    
+    
+} //end of class
 
